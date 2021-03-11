ca0ab87f7107194fafafa26a8d3492c4aa855300
kulwatee <kulwatee.j@ku.th>
2021-03-11 15:08:19 +0700

    1

---
+++ 6320502355_2.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        if(n%2!=0)
+        {
+           for(j=0;j<n;j++)
+           {
+
+           }
+        }
+        else
+        {
+           for(w=0;w<n-1;w++)
+           {
+
+           }
+        }
+    }
+}
