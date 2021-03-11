51f037abdcbaad2b4deab5ac2b07f5d5dc5b367f
kulwatee <kulwatee.j@ku.th>
2021-03-11 17:11:35 +0700

    9

--- 6320502355_5.c
+++ 6320502355_5.c
@@ -1,10 +1,27 @@
 #include<stdio.h>
 int main()
 {
-    int a,b;
+    int a,b,i,j,sum,w,e;
     scanf("%d",&a);
     scanf("%d",&b);
     int x[a][a];
-    int y[b][b];
-
+    for(i=0;i<a;i++)
+    {
+        for(j=0;j<a;j++)
+        {
+            scanf("%d",&x[i][j]);
+        }
+    }
+    for(i=0;i<a-b+1;i++)
+    {
+        for(j=0;j<a-b+1;j++)
+        {
+           sum=0;
+           for(w=i;w<b+j;w++)
+           {
+               for(e=j;e<b+j;e++)
+                sum+=x[w][e];
+           }
+        }
+    }
 }
