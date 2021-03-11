0b198d255c6366b18507d833ab19047741b46820
kulwatee <kulwatee.j@ku.th>
2021-03-11 15:54:20 +0700

    4

--- 6320502355_2.c
+++ 6320502355_2.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,j,q=0;
+    int n,i,j,q=0,r=0;
     scanf("%d",&n);
 
     if(n%2!=0)
@@ -10,17 +10,22 @@ int main()
         {
             for(j=n; j>0; j--)
             {
-                if(j==(n/2)-q||j==(n/2)+q)
-                {
+                if(j==(n/2)-q+1||j==(n/2)+r+1)
                     printf("*");
-                    break;
-                }
-                printf("_");
+                else
+                    printf("_");
             }
+            printf("\n");
             if(i<n/2)
+            {
                 q++;
+                r++;
+            }
             else
+            {
                 q--;
+                r--;
+            }
         }
     }
     else
@@ -29,17 +34,22 @@ int main()
         {
             for(j=0; j<n-1; j++)
             {
-                if(j==(n/2)-q||j==(n/2)+q)
-                {
+                if(j==(n/2)-q-1||j==(n/2)+r-1)
                     printf("*");
-                    break;
-                }
-                printf("_");
+                else
+                    printf("_");
             }
+            printf("\n");
             if(i<(n/2)-1)
+            {
                 q++;
-            else if(i>n/2)
+                r++;
+            }
+            else if(i>=n/2)
+            {
                 q--;
+                r--;
+            }
         }
 
     }
